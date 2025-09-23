c619109c62a32db9a0158e5d4eaed233f39c045d
Ved_V <78527436+Tommy12356F@users.noreply.github.com>
2025-09-24 03:59:24 +0530

    Time: 4 ms (81.43%) | Memory: 12.4 MB (80.71%) - LeetSync

---
+++ 2767-maximum-sum-with-exactly-k-elements/maximum-sum-with-exactly-k-elements.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maximizeSum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        return max(nums)*k + (k**2 - k)/2
